afff53f1147f7a1c4812918bf9a33bae5500b557
k-atherton <40177040+k-atherton@users.noreply.github.com>
2018-06-29 14:08:44 -0400

    Update to loop through each taxonomic data set

--- data_construction/EMP_data_construction/7._subset_50percent.r
+++ data_construction/EMP_data_construction/7._subset_50percent.r
@@ -3,139 +3,33 @@
 #Finally, try rewriting this as a loop operating on each taxonomic data set stored in a list.
 rm(list=ls())
 source('paths.r')
-#setwd("~/Desktop/NEFI_16S_data/")
 
 #load the relative taxonomic data
-  phyla <- readRDS(emp_phylum_esv.path)
-classes <- readRDS( emp_class_esv.path)
-  order <- readRDS( emp_order_esv.path)
- family <- readRDS(emp_family_esv.path)
- genera <- readRDS( emp_genus_esv.path)
+data <- readRDS(emp_phylo.level.list_esv.path)
 
 # find number for half the samples
-n_half <- nrow(phyla)/2 #same for every level of taxonomy
+n_half <- ncol(data[[1]])/2 #same for every level of taxonomy
 
-### PHYLA ###
-#loop setup
-count <- 0 
-num <- 1
-names <- character()
-#loop through and count number of samples taxa are in
-for(i in 1:ncol(phyla)){
-  for(j in 1:nrow(phyla)){
-    if(phyla[j,i] > 0){
-      count = count + 1
+#loop through each data set to subset out only taxa found in 50%+ of samples
+for(i in 1:length(data.list)){
+  count <- 0 
+  num <- 1
+  names <- character()
+  #loop through and count number of samples taxa are in
+  for(j in 1:nrow(data.list[[i]])){ #rows are taxa
+    for(k in 1:ncol(data.list[[i]])){ #columns are samples
+      if(data.list[[i]][j,k] > 0){
+        count = count + 1
+      }
     }
-  }
-  if(count >= n_half){
-    names[num] <- colnames(phyla)[i]
-    num = num + 1
-  }
-  count = 0
-}
-#subset out chosen taxa
-phyla <- phyla[names]
-colnames(phyla) <- gsub("[.]","-",colnames(phyla))
-
-
-### CLASS ###
-#loop setup
-count <- 0 
-num <- 1
-names <- character()
-#loop through and count number of samples taxa are in
-for(i in 1:ncol(classes)){
-  for(j in 1:nrow(classes)){
-    if(classes[j,i] > 0){
-      count = count + 1
+    if(count >= n_half){
+      names[num] <- rownames(data.list[[i]])[j]
+      num = num + 1
     }
+    count = 0
   }
-  if(count >= n_half){
-    names[num] <- colnames(classes)[i]
-    num = num + 1
-  }
-  count = 0
+  data.list[[i]] <- data.list[[i]][names,]
 }
-#subset out chosen taxa
-classes <- classes[names]
-colnames(classes) <- gsub("[.]","-",colnames(classes))
-colnames(classes)[1] <- gsub("V","",colnames(classes)[1])
-colnames(classes)[2:5] <- gsub("X","",colnames(classes)[2:5])
-
-### ORDER ###
-#loop setup
-count <- 0 
-num <- 1
-names <- character()
-#loop through and count number of samples taxa are in
-for(i in 1:ncol(order)){
-  for(j in 1:nrow(order)){
-    if(order[j,i] > 0){
-      count = count + 1
-    }
-  }
-  if(count >= n_half){
-    names[num] <- colnames(order)[i]
-    num = num + 1
-  }
-  count = 0
-}
-#subset out chosen taxa
-order <- order[names]
-colnames(order) <- gsub("[.]","-",colnames(order))
-colnames(order)[1] <- gsub("V","",colnames(order)[1])
-colnames(order)[2:6] <- gsub("X","",colnames(order)[2:6])
-
-### FAMILY ###
-#loop setup
-count <- 0 
-num <- 1
-names <- character()
-#loop through and count number of samples taxa are in
-for(i in 1:ncol(family)){
-  for(j in 1:nrow(family)){
-    if(family[j,i] > 0){
-      count = count + 1
-    }
-  }
-  if(count >= n_half){
-    names[num] <- colnames(family)[i]
-    num = num + 1
-  }
-  count = 0
-}
-#subset out chosen taxa
-family <- family[names]
-colnames(family) <- gsub("[.]","-",colnames(family))
-colnames(family)[1] <- gsub("V","",colnames(family)[1])
-colnames(family)[2:6] <- gsub("X","",colnames(family)[2:6])
-
-### GENERA ###
-#loop setup
-count <- 0 
-num <- 1
-names <- character()
-#loop through and count number of samples taxa are in
-for(i in 1:ncol(genera)){
-  for(j in 1:nrow(genera)){
-    if(genera[j,i] > 0){
-      count = count + 1
-    }
-  }
-  if(count >= n_half){
-    names[num] <- colnames(genera)[i]
-    num = num + 1
-  }
-  count = 0
-}
-#subset out chosen taxa
-genera <- genera[names]
-colnames(genera) <- gsub("[.]","-",colnames(genera))
-colnames(genera)[1] <- gsub("V","",colnames(genera)[1])
 
 #save as RDS
-saveRDS(phyla, "phyla_rel_abundance_50.RDS")
-saveRDS(classes, "class_rel_abundance_50.RDS")
-saveRDS(order, "order_rel_abundance_50.RDS")
-saveRDS(family, "family_rel_abundance_50.RDS")
-saveRDS(genera, "genera_rel_abundance_50.RDS")
+saveRDS(data.list, emp_phylo.level.list_esv.path)
